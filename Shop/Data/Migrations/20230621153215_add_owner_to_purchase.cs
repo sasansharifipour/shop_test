a9d7b56d1aba625b4b1e456df369c5aeca2518c1
Sasan Sharifipour <sasansharifipour@gmail.com>
2023-06-21 19:04:21 +0330

    add migrations files

---
+++ Shop/Data/Migrations/20230621153215_add_owner_to_purchase.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Shop.Data.Migrations
+{
+    public partial class add_owner_to_purchase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerId",
+                table: "Purchases",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Purchases_OwnerId",
+                table: "Purchases",
+                column: "OwnerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Purchases_AspNetUsers_OwnerId",
+                table: "Purchases",
+                column: "OwnerId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Purchases_AspNetUsers_OwnerId",
+                table: "Purchases");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Purchases_OwnerId",
+                table: "Purchases");
+
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                table: "Purchases");
+        }
+    }
+}
